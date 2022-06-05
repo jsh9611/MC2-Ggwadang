131567127c950acd00d7b66a8202661438c66ab9
JIMIN KIM <jim4020key@gmail.com>
2022-06-05 22:59:54 +0900

    Initial Commit

---
+++ ggwadang/ggwadangApp.swift
@@ -0,0 +1,17 @@
+//
+//  ggwadangApp.swift
+//  ggwadang
+//
+//  Created by kimjimin on 2022/06/05.
+//
+
+import SwiftUI
+
+@main
+struct ggwadangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
