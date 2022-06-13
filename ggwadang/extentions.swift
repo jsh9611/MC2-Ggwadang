8586b90f7cc33e0f4c3b47872cad304e48324233
jsh9611 <jangsh9611@naver.com>
2022-06-13 20:46:32 +0900

    Add extentions for Color extention

---
+++ ggwadang/extentions.swift
@@ -0,0 +1,22 @@
+//
+//  extentions.swift
+//  GwadangAntony
+//
+//  Created by SeongHoon Jang on 2022/06/09.
+//
+
+import SwiftUI
+
+// Color에 기능을 추가하였다.
+// Color(hex: 0x6CADA5) 와 같이 hex 값으로 색을 바로 쓸 수 있음.
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
