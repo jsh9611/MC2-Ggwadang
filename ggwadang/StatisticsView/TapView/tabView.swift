e7b9baa0139d5a8fd48448b07c145c66bf0c374e
한형서 <2017029243@hanyang.ac.kr>
2022-06-19 01:04:29 +0900

    Integrate ListView to TabView

--- ggwadang/StatisticsView/TapView/tabView.swift
+++ ggwadang/StatisticsView/TapView/tabView.swift
@@ -9,32 +9,58 @@ import SwiftUI
 
 struct tabView: View {
     @State private var selected = 0
+    @State private var oldSelected = 0
+    @EnvironmentObject var store: RecordStore
+    @State var isPresented = false
+    
+    let records: [Record]
+    
     var body: some View {
         VStack{
             TabView(selection: $selected){
                 testView()
                     .tabItem {
                         Image(systemName:(selected == 0 ? "house.fill" : "house"))
-                    }.tag(0)
-                Text("shibal")
+                    }
+                    .tag(0)
+                    .onAppear{
+                        self.oldSelected = self.selected
+                    }
+                
+                Text("")
                     .tabItem {
                         Image(systemName: "plus")
                             .foregroundColor(Color("CustomColor"))
                             .background(Color("CustomColor"))
-                    }.tag(1)
+                    }
+                    .onAppear{
+                        isPresented.toggle()
+                        self.selected = self.oldSelected
+                    }
+                    .tag(1)
+                
                 SegView()
                     .tabItem {
                         Image(systemName: (selected == 2 ? "chart.bar.fill" : "chart.bar"))
-                    }.tag(2)
+                    }
+                    .tag(2)
+                    /*
+                    .onAppear{
+                        self.oldSelected = self.selected
+                    }
+                    */
                 }
+                .fullScreenCover(isPresented: $isPresented, content: {LargeCategoryView(isPresented: self.$isPresented).environmentObject(self.store)})
         }
     }
 }
 //.onChange
 
+/*
 struct tabView_Previews: PreviewProvider {
     static var previews: some View {
         tabView()
     }
 }
+*/
 
