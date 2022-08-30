50f038e41780de2c9db225180a91a0bee3e17e11
jsh9611 <jangsh9611@naver.com>
2022-08-30 23:47:37 +0900

    [Refactor]도움말 페이지가 NavigationLink 형태로 나오도록 변경

--- ggwadang/Views/Settings/SettingListView.swift
+++ ggwadang/Views/Settings/SettingListView.swift
@@ -9,24 +9,39 @@ import SwiftUI
 
 struct SettingListView: View {
     
-    // 사용자 안내 온보딩 페이지
+    // MARK: - AppStorage Keys for Onboarding(도움말)
     @AppStorage(StorageKeys.isFirstLaunching.rawValue) private var isFirstLaunching: Bool = true
-    // 사용자 입력 온보딩 페이지(테스트용이라 나중에 무조건 빼주세요!!!)
-    @AppStorage(StorageKeys.isFirstDataInput.rawValue) private var isFirstDataInput: Bool = true   // 유저 입력
     @Binding var naviLinkActive : Bool
     
     var body: some View {
         List {
-            Section {
+            Section(header: Text("사용자")) {
                 NavigationLink(destination: SettingUserInfoView(naviLinkActive: $naviLinkActive)) {
                     Text("개인정보 변경")
                 }
+                
                 NavigationLink(destination: SettingUserSugarView(naviLinkActive: $naviLinkActive)) {
                     Text("목표 섭취량 변경")
                 }
-                Toggle("도움말 다시보기", isOn: $isFirstLaunching)
             }
+            .listRowBackground(Color.clear)
+            
             Section(header: Text("정보")) {
+                ZStack {
+                    NavigationLink(destination: Text("짜잔!")) {
+                        Text("도움말 다시보기")
+                    }
+                    
+                    HStack {
+                        VStack {
+                            Rectangle().opacity(0.00000001)
+                            Spacer()
+                        }
+                        Spacer()
+                    }
+                    
+                }.onTapGesture { isFirstLaunching.toggle() }
+
                 NavigationLink(destination: LicenseView()) {
                     Text("오픈소스 라이선스")
                 }
@@ -36,6 +51,6 @@ struct SettingListView: View {
         .onAppear{
             UITableView.appearance().backgroundColor = UIColor.white
         }
-        .navigationBarTitle("Settings")
+        .navigationBarTitle("설정")
     }
 }
